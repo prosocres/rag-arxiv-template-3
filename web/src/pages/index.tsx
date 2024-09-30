86c33465f57e49cfcc0b96d287e867e201c581ac
prosocres <20094619+prosocres@users.noreply.github.com>
2024-09-29 22:13:55 -0400

    Progresses the form component

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,16 +1,30 @@
+import { z } from "zod"
 import { Button } from "@/components/ui/button";
 import { Form, FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { pages } from "next/dist/build/templates/app-page";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const submitPaperFormSchema = z.object({
+  paperUrl: z.string(),
+  name: z.string(),
+  pagesToDelete: z.array(z.number()).optional(),
+})
 
 export default function Home() {
+  const submitPaperForm = useForm<z.infer<typeof submitPaperFormSchema>>({
+    resolver: zodResolver(submitPaperFormSchema),
+  })
+  
   return (
     <div className="flex flex-row gap-5">
       {/** Add paper */}
       <div className="flex flex-col gap-2 border-[1px] border-gray-400 rounded-md"></div>
-        <Form {...form}>
+        <Form {...submitPaperForm}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
-            control={form.control}
+            control={submitPaperForm.control}
             name="username"
             render={({ field }) => (
               <FormItem>
