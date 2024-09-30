8a89dd7b647578778e2196fdae592eb5e613c0b5
prosocres <20094619+prosocres@users.noreply.github.com>
2024-09-29 22:00:38 -0400

    Adds input component

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Form, FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   return (
-    <div className="flex flex-col gap-5">
-      Hello world
+    <div className="flex flex-row gap-5">
+      {/** Add paper */}
+      <div className="flex flex-col gap-2 border-[1px] border-gray-400 rounded-md"></div>
+        <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+      {/** QA on paper */}
+      <div></div>
     </div>
   );
 }
